a6d98f530f425569fbeca6dc3b6f15a4a2dcc7ed
smith1967 <smith.sookkhi@gmail.com>
2014-05-11 13:36:54 +0700

    update phalcon framework started

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -12,5 +12,19 @@ class IndexController extends \Phalcon\Mvc\Controller {
 //        $this->view->setVar("email", $user->getEmail());
         $this->view->items = Users::find();
     }
-
+    public function saveAction() {
+//        echo 'Save Action';
+        $data = array(
+            'name'=>'somkid',
+            'email' => 'somkid@cstc.ac.th'
+        );
+        $user = new Users();
+        $user->assign($data);
+        $user->save();
+    }
+    public function deleteAction() {
+        //echo 'Delete Action';
+        $user = Users::findFirst("id=3");
+        $user->delete();
+    }
 }
